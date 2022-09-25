95c5a44a440033960d0a70bbe5ca23cdfe820df2
Adriana Rendon <nanire12@hotmail.com>
2022-09-25 00:39:00 -0500

    Create Java Project

---
+++ Control_Temperatura/src/main/java/com/app/web/Control_TemperaturaApplication.java
@@ -0,0 +1,13 @@
+package com.app.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Control_TemperaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Control_TemperaturaApplication.class, args);
+	}
+
+}
