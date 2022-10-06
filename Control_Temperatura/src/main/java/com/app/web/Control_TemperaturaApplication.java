3f7de0dbada9266f5c3db9fa4640736612b802d2
Adriana Rendon <nanire12@hotmail.com>
2022-10-05 20:30:10 -0500

    Edit user

--- Control_Temperatura/src/main/java/com/app/web/Control_TemperaturaApplication.java
+++ Control_Temperatura/src/main/java/com/app/web/Control_TemperaturaApplication.java
@@ -20,11 +20,11 @@ public class Control_TemperaturaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Usuarios usuario1 = new Usuarios(null, null, null, "Cadena1", "Area1", 12, "Sin observacion1");
+		/*Usuarios usuario1 = new Usuarios(null, null, null, "Cadena1", "Area1", 12, "Sin observacion1");
 		repositorio.save(usuario1);
 
 		Usuarios usuario2 = new Usuarios(null, null, null, "Cadena2", "Area2", 12, "Sin observacion2");
-        repositorio.save(usuario2);
+        repositorio.save(usuario2);*/
 		
 	}
 
